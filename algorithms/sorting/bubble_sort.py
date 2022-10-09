e29afbbab9bd2af17315052bd42943bb54be11d2
Samuel Valentine <valentinesamuel2580@gmail.com>
2022-10-09 14:32:21 +0000

    added bubble sort and insertion sort

---
+++ algorithms/sorting/bubble_sort.py
@@ -0,0 +1,29 @@
+def bubbleSort(arr):
+	for i in range(len(arr)):
+		for j in range(len(arr) - 1):
+			if arr[j] > arr[j+1]:
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+	return arr
+
+# def bubbleSort2(arr):
+# 	for i in range(len(arr)):
+# 		for j in range(len(arr) - i - 1):
+# 			if arr[j] > arr[j+1]:
+# 				arr[j], arr[j+1] = arr[j+1], arr[j]
+# 	return arr
+
+# def bubbleSort3(arr):
+# 	for i in range(len(arr)):
+# 		flag = 0
+# 		for j in range(len(arr) - i - 1):
+# 			if arr[j] > arr[j+1]:
+# 				arr[j], arr[j+1] = arr[j+1], arr[j]
+# 				flag = 1
+# 		if(flag == 0):
+# 			break
+# 	return arr
+
+
+
+lst = [2, 4, 7, 2, 6, 8, 12, 1, 0]
+print(bubbleSort(lst))
--- algorithms/sorting/selection_sort.py
+++ algorithms/sorting/selection_sort.py
@@ -1,2 +1,13 @@
-def selectionSort():
-	pass
+def selectionSort(arr, arr2):
+	for current_element in range(len(arr)):
+		current_minimum = current_element
+		for smallest in range(current_element + 1, len(arr2)):
+			if arr[current_minimum] > arr[smallest]:
+				current_minimum = smallest
+		arr[current_element], arr[current_minimum] = arr[current_minimum], arr[
+		 current_element]
+	return arr
+
+
+lst = [2, 4, 7, 2, 6, 8, 12, 1, 0]
+print(selectionSort(lst))
