42cc2b9d90c04bcfa894780c422282b41a989f99
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-21 08:23:13 +0000

    FEAT: Safeguarded my bubblesort

--- test_algo/bubble_sort.py
+++ test_algo/bubble_sort.py
@@ -2,7 +2,7 @@ numbers = [99,46,6,2,1,5,63,87,283,4,0]
 
 def bubbleSort(array):
     for idx1 in range(len(array)):
-        for idx in range(len(array)-1):
+        for idx in range(0,len(array)-idx1-1):
             if array[idx] > array[idx+1]:
                 array[idx], array[idx+1] = array[idx+1], array[idx]
     print(array)
