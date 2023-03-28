5b1fa620b992ad2f60f7abc1caf97404e6f882ce
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-28 16:39:20 +0100

    FEAT: created mergesort on my own

--- playground.py
+++ playground.py
@@ -8,17 +8,44 @@ def insertionSort2(arr):
         arr[idx2+1] = temp
     return arr
 
+def bubbleSort(arr):
+    for idx in range(len(arr)):
+        for idx2 in range(len(arr)-idx-1):
+            if arr[idx2] > arr[idx2+1]:
+                arr[idx2], arr[idx2+1] = arr[idx2+1], arr[idx2]
+    return arr
+
+def insertionSort(arr):
+    for idx in range(1, len(arr)):
+        currmin = idx
+        while currmin > 0 and arr[currmin-1] > arr[currmin]:
+            arr[currmin], arr[currmin-1] = arr[currmin-1], arr[currmin]
+            currmin -=1
+    return arr
+
+
+def selectionSort(arr):
+    for idx1 in range(len(arr)):
+        min = idx1
+        for idx2 in range(idx1,len(arr)):
+            if arr[idx2] < arr[min]:
+                min = idx2
+        arr[idx1], arr[min] = arr[min], arr[idx1]
+    return arr
+
 def mergeSort(arr):
     if len(arr) <= 1:
         return arr
-    
+
     mid = len(arr) // 2
+
     left = mergeSort(arr[:mid])
     right = mergeSort(arr[mid:])
 
     return merge(left, right)
-
+    
 def merge(left, right):
+
     result = []
     i, j = 0, 0
 
@@ -29,11 +56,12 @@ def merge(left, right):
         else:
             result.append(right[j])
             j += 1
-        
+
     result += left[i:]
     result += right[j:]
-
+        
     return result
 
-print(mergeSort([99,44,6,2,1,5,63,87,283,4,0]))
-print(mergeSort([13, 4, 9, 5, 3, 16, 12]))
+
+print(insertionSort([99,44,6,2,1,5,63,87,283,4,0]))
+print(insertionSort([13, 4, 9, 5, 3, 16, 12]))
---
+++ test_algo/merge_sort.py
@@ -0,0 +1,29 @@
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left = mergeSort(arr[:mid])
+    right = mergeSort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    
+    result += left[i:]
+    result += right[j:]
+
+    return result
+
+print(mergeSort([99,44,6,2,1,5,63,87,283,4,0]))
+print(mergeSort([13, 4, 9, 5, 3, 16, 12]))
