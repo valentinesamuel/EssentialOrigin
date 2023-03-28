67849cb51cd81f2182634fd66296b63a864e1763
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-28 15:35:56 +0100

    Feat: Understanding mergesort

--- playground.py
+++ playground.py
@@ -1,21 +1,3 @@
-def selectionSort(arr):
-    for idx1 in range(len(arr)):
-        min = idx1
-        for idx2 in range(idx1+1, len(arr)):
-            if arr[idx2] < arr[min]:
-               min = idx2
-        arr[idx1], arr[min] = arr[min], arr[idx1]
-    return arr
-        
-
-def insertionSort(arr):
-    for idx1 in range(1, len(arr)):
-        current = idx1
-        while current > 0 and arr[current - 1] > arr[current]:
-            arr[current], arr[current-1] = arr[current-1], arr[current]
-            current = current - 1
-    return arr
-
 def insertionSort2(arr):
     for idx in range(1, len(arr)):
         temp = arr[idx]    
@@ -26,14 +8,32 @@ def insertionSort2(arr):
         arr[idx2+1] = temp
     return arr
 
-def bubbleSort(arr):
-    for idx1 in range(len(arr)):
-      for idx2 in range(len(arr)-idx1-1):
-         if arr[idx2] > arr[idx2+1]:
-            arr[idx2+1], arr[idx2] = arr[idx2], arr[idx2+1]
-    return arr
-
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
     
+    mid = len(arr) // 2
+    left = mergeSort(arr[:mid])
+    right = mergeSort(arr[mid:])
 
-print(insertionSort([99,44,6,2,1,5,63,87,283,4,0]))
-print(insertionSort([13, 4, 9, 5, 3, 16, 12]))
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+        
+    result += left[i:]
+    result += right[j:]
+
+    return result
+
+print(mergeSort([99,44,6,2,1,5,63,87,283,4,0]))
+print(mergeSort([13, 4, 9, 5, 3, 16, 12]))
