948a65b0bf5c641406392406b5e5b2038ab22ba3
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-29 09:16:09 +0100

    Implemented bubble, selection, insertion and merge sorting algorithms with little help

--- playground.py
+++ playground.py
@@ -1,37 +1,53 @@
-def insertionSort2(arr):
+def insertionSort(arr):
     for idx in range(1, len(arr)):
-        temp = arr[idx]    
-        idx2 = idx-1
-        while (idx2 >= 0 and  arr[idx2] > temp):
-            arr[idx2+1] = arr[idx2]
-            idx2-=1
-        arr[idx2+1] = temp
+        curr_min = idx
+        while curr_min > 0 and arr[curr_min - 1] > arr[curr_min]:
+            arr[curr_min], arr[curr_min - 1] = arr[curr_min - 1], arr[curr_min]
+            curr_min -= 1
     return arr
 
 def bubbleSort(arr):
-    for idx in range(len(arr)):
-        for idx2 in range(len(arr)-idx-1):
+    for idx1 in range(len(arr)):
+        for idx2 in range(len(arr) - idx1 - 1):
             if arr[idx2] > arr[idx2+1]:
-                arr[idx2], arr[idx2+1] = arr[idx2+1], arr[idx2]
-    return arr
-
-def insertionSort(arr):
-    for idx in range(1, len(arr)):
-        currmin = idx
-        while currmin > 0 and arr[currmin-1] > arr[currmin]:
-            arr[currmin], arr[currmin-1] = arr[currmin-1], arr[currmin]
-            currmin -=1
+                arr[idx2], arr[idx2 + 1] = arr[idx2 + 1], arr[idx2]
     return arr
 
 def selectionSort(arr):
     for idx1 in range(len(arr)):
-        min = idx1
-        for idx2 in range(idx1,len(arr)):
-            if arr[idx2] < arr[min]:
-                min = idx2
-        arr[idx1], arr[min] = arr[min], arr[idx1]
+        current_min = idx1
+        for idx2 in range(idx1+1, len(arr)):
+            if arr[idx2] < arr[current_min]:
+                current_min = idx2
+        arr[idx1], arr[current_min] = arr[current_min], arr[idx1]
     return arr
 
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
 
-print(insertionSort([99,44,6,2,1,5,63,87,283,4,0]))
-print(insertionSort([13, 4, 9, 5, 3, 16, 12]))
+    mid = len(arr) // 2
+    left = mergeSort(arr[:mid])
+    right = mergeSort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i,j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    
+    result+=left[i:]
+    result+=right[j:]
+
+    return result
+
+print(mergeSort([99,44,6,2,1,5,63,87,283,4,0]))
+print(mergeSort([13, 4, 9, 5, 3, 16, 12]))
