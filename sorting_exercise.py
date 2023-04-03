c973536afe6095f3b6263cf3a58655a6515a41be
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-03 14:22:14 +0100

    FEAT: Added sorting exercises

---
+++ sorting_exercise.py
@@ -0,0 +1,15 @@
+# Sort 10 schools around your house by distance
+
+# Sort eBay listings by the current bid amount
+
+# Sort scores on ESPN
+
+# Sort a masssive database (can't fit all into memory). It needs to sort past year's data
+
+# Almost sorted Udemy review data needs to update and add 2 new reviews
+
+# Temperature Records for the past 50 years in Canada
+
+# Large username database needs to be sorted. Data is very random
+
+# Trying to teach sorting for the first time
