0e5a33b9c41c50f3c4241c6960f5d687909f2ec6
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-03 10:29:11 +0100

    FEAT: Practiced selection  sort

--- playground.py
+++ playground.py
@@ -6,6 +6,15 @@ def insertionSort(arr):
             traverser -= 1
     return arr
 
+def selectionSort(arr):
+    for idx1 in range(len(arr)):
+        curr_min = idx1
+        for idx2 in range(idx1+1, len(arr)):
+            if arr[idx2] < arr[curr_min]:
+                curr_min = idx2
+        arr[idx1], arr[curr_min] = arr[curr_min], arr[idx1] 
+    return arr
 
-print(insertionSort([99,44,6,2,1,5,63,87,283,4,0]))
-print(insertionSort([13, 4, 9, 5, 3, 16, 12]))
+
+print(selectionSort([99,44,6,2,1,5,63,87,283,4,0]))
+print(selectionSort([13, 4, 9, 5, 3, 16, 12]))
