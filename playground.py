d83058f9d297e6ba05dd1cc1936f9c256e7eaec7
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-03 10:30:04 +0100

    FEAT: Practiced merge sort

--- playground.py
+++ playground.py
@@ -22,6 +22,32 @@ def bubbleSort(arr):
                 arr[idx2], arr[idx2+1] = arr[idx2+1], arr[idx2]
     return arr
 
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left = mergeSort(arr[:mid])
+    right = mergeSort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    
+    result += left[i:]
+    result += right[j:]
+
+    return result
 
 
 print(selectionSort([99,44,6,2,1,5,63,87,283,4,0]))
