bfd8b501a5a8079be18e3e61787d6e7d7d558959
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-03 13:11:45 +0100

    FEAT: Made range to run from low to high+1

--- playground.py
+++ playground.py
@@ -1,24 +1,16 @@
-def partition1(arr, high, low=0):
+def partition(arr, high, low=0):
     if not arr and not high:
         return 'No array and no upperbound'
 
-
     pivot = arr[high]
     partion_at = low
-    for j in range(low, high):
+    for j in range(low, high+1):
         if arr[j] <= pivot:
             arr[partion_at], arr[j] = arr[j], arr[partion_at]
             partion_at += 1
     arr[partion_at], arr[high] = arr[high], arr[partion_at]  
     return partion_at
 
-def partition(array, low, high):
-            pivot = array[high]
-            partat = low
-            for i in range(low, high):
-                if array[i] <= pivot:
-                    array[partat], array[i] = array[i], array[partat]
-                    partat = partat + 1
 
-            array[partat], array[high] = array[high], array[partat]
-            return partat
+unsorted_arr = [7,2,1,8,6,3,5,4]
+print(partition(unsorted_arr,len(unsorted_arr)-1))
