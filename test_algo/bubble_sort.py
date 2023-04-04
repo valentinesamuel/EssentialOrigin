b17310e1f27d018240d04b3a7a0f3f5cbd544509
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-04 16:55:13 +0100

    FAET: Added test for bubblesort

--- test_algo/bubble_sort.py
+++ test_algo/bubble_sort.py
@@ -1,19 +1,27 @@
+import unittest
+
 numbers = [99,46,6,2,1,5,63,87,283,4,0]
 
 def bubbleSort(array):
+
+    if not isinstance(array,list):
+        raise TypeError
+
     for idx1 in range(len(array)):
         for idx in range(0,len(array)-idx1-1):
             if array[idx] > array[idx+1]:
                 array[idx], array[idx+1] = array[idx+1], array[idx]
-    print(array)
+    return array
 
-# def selectionSort(arr):
-#     for idx in range(len(arr)):
-#         min = idx
-#         for idx1 in range(idx+1,len(arr)):
-#             if arr[idx1] < arr[min]:
-#                 min = idx1
-#         arr[idx], arr[min] = arr[min],  arr[idx]
-#     print(arr)
+# bubbleSort(numbers)
 
-bubbleSort(numbers)
+class TestBubbleSort(unittest.TestCase):
+    def test_bubbleSort(self):
+        self.assertEqual(bubbleSort([2,1,5,63,87]), [1,2,5,63,87])
+
+    def test_bubbleSort_non_array(self):
+        with self.assertRaises(TypeError):
+            bubbleSort('[2,1,5,63,87]')
+
+if __name__ == '__main__':
+    unittest.main()
