6eaffbc52cedd1e6b30de3c94dd6a0ee8411e2ac
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-05 10:32:17 +0100

    FEAT: Practiced bfs recursive and dfs

--- playground.py
+++ playground.py
@@ -1,210 +1,27 @@
-class Node:
-    def __init__(self, value):
-        self.left = None
-        self.right = None
-        self.value = value
-
-    def __repr__(self):
-        return str(self.__dict__)
+def quickSort(arr, left, right):
+    if len(arr) <= 1:
+        return arr
     
-# from collections import deque
+    if left < right:
+        partition_idx = partition(arr, left, right)
+        left = quickSort(arr, left, partition_idx-1)
+        right = quickSort(arr, partition_idx+1, right)
 
-class BinarySearchTree():
-    def __init__(self) -> None:
-        self.root = None
+    return arr
 
-    def __repr__(self) -> None:
-        return str(self.__dict__)
+def partition(arr, left, right):
+    pivot = arr[right]
+    part_at = left
 
-    def insert(self, value):
-        newNode = Node(value)
-        if self.root == None:
-            self.root = newNode
-            return
-        currentNode = self.root
-        while True:
-            if currentNode.value > newNode.value:
-                if currentNode.left == None:
-                    currentNode.left = newNode
-                    return
-                currentNode = currentNode.left
-            if currentNode.value < newNode.value:
-                if currentNode.right == None:
-                    currentNode.right = newNode
-                    return
-                currentNode = currentNode.right        
-
-    def lookup(self, value):
-        if self.root == None:
-            print("Empty Tree")
-            return
-        currentNode = self.root
-        while currentNode:
-            if currentNode.value > value:
-                currentNode = currentNode.left
-            elif currentNode.value < value:
-                currentNode = currentNode.right
-            elif currentNode.value == value:
-                print("Found")
-                return currentNode
-        print("Not Found")
-        return None
-    
-    def remove(self, value):
-        if self.root == None:
-            return "Empty List"
-        currentNode = self.root
-        parentNode = None
-        while currentNode != None:
-            if currentNode.value > value:
-                parentNode = currentNode
-                currentNode = currentNode.left
-            elif currentNode.value < value:
-                parentNode = currentNode
-                currentNode = currentNode.right
-            else: # A match has been found
-                # Both left and right children
-                if currentNode.left != None and currentNode.right != None:
-                    badNode = currentNode.right
-                    badNodeParent = currentNode.right
-                    while badNode.left != None:
-                        # Fetch the leftmost node of the right subtree of the current node
-                        badNodeParent = badNode
-                        badNode = badNode.left
-                    currentNode.value = badNode.value
-                    # Copy the values from the found node to the current node
-                    if badNode == badNodeParent:
-                        # if the badnode is the rightmost child of the currentnode,link the currentnode to the right node of the bad node
-                        currentNode.right = badNode.right
-                    if badNode.right == None:
-                        # if the leftmost node of the right subtree of the currentnode has no right subtree. N/B that this left most node is a leaf node
-                        badNodeParent.left = None
-                        return
-                    else:
-                        # if the leftmost node of the right subtree of the currentnode has a  right subtree. N/B that this left most node has no left children but only a right subtree (child/children)
-                        badNodeParent.left = badNode.right
-                        return
-                
-                # No left or right children
-                elif currentNode.left == None and currentNode.right == None:
-                    if parentNode == None:
-                        # if we didn't move from tne root Node  
-                        self.root = None
-                        return
-                    # Since the currentnode is a leaf node with no child, both sides will be empty
-                    if parentNode.value > currentNode.value:
-                        parentNode.left = None
-                        return
-                    else:
-                        parentNode.right = None
-                        return
-
-                # Only left child
-                elif currentNode.left != None and currentNode.right == None:
-                    # if we didn't move from tne root Node  
-                    if parentNode == None:
-                        self.root = None
-                        return
-                    if parentNode.value > currentNode.value:
-                        # link the parent node to the node after the currentnode to keep everything in order
-                        parentNode.left = currentNode.left
-                        return
-                    else:
-                        # since the currentnode is bigger than the parent node, the left node after the current node will be bigger than the parent node so it has to be on the right of the parent node so that the right node after the current node will be correct in its position by remaining on the right of the new currentnode
-                        parentNode.right = currentNode.left
-                        return
-
-                # Only right child
-                elif currentNode.left == None and currentNode.right != None:
-                    if parentNode == None:
-                        self.root = None
-                        return
-                    if parentNode.value > currentNode.value:
-                        parentNode.left = currentNode.right
-                        return
-                    else:
-                        parentNode.right = currentNode.right
-                        return
-        return ("Not Found")        
-        
-    def breadthFirstSearch(self):
-        current_node = self.root
-        result = []
-        queue = [] # queue = deque()
-        queue.append(current_node) 
-
-        while len(queue) > 0:
-            current_node = queue.pop(0) # queue.popleft()
-            result.append(current_node.value)
-            if current_node.left:
-                queue.append(current_node.left)
-            if current_node.right:
-                queue.append(current_node.right)
-        
-        return result
-        
-    def breadthFirstSearchRecursive(self, queue, result):
-        if not len(queue):
-            return result
-        
-        curr_node = queue.pop(0)
-        result.append(curr_node.value)
-        if curr_node.left:
-            queue.append(curr_node.left)
-        if curr_node.right:
-            queue.append(curr_node.right)
-        
-        return self.breadthFirstSearchRecursive(queue,result)
-
-    def depthFirstSearchInOrder(self):
-        return traverseInOrder(self.root, [])
-
-    def depthFirstSearchPreOrder(self):
-        return traversePreOrder(self.root, [])
-
-    def depthFirstSearchPostOrder(self):
-        return traversePostOrder(self.root, [])
+    for j in range(left, right):
+        if arr[j] < pivot:
+            arr[part_at], arr[j] = arr[j], arr[part_at]
+            part_at += 1
+    arr[part_at], arr[right] = arr[right], arr[part_at]
+    return part_at
 
 
-def traverseInOrder(node,result):
-    if node.left:
-        traverseInOrder(node.left, result)
-    result.append(node.value)
-    if node.right:
-        traverseInOrder(node.right, result)
-    return result
-
-def traversePreOrder(node,result):
-    result.append(node.value)
-    if node.left:
-        traversePreOrder(node.left, result)
-    if node.right:
-        traversePreOrder(node.right, result)
-    return result
-
-def traversePostOrder(node,result):
-    if node.left:
-        traversePostOrder(node.left, result)
-    if node.right:
-        traversePostOrder(node.right, result)
-    result.append(node.value)
-    return result
-
-
-bst = BinarySearchTree()
-bst.insert(9)
-bst.insert(4)
-bst.insert(6)
-bst.insert(20)
-bst.insert(170)
-bst.insert(15)
-bst.insert(1)
-# bst.insert(10)
-# print(bst)
-# print(bst.breadthFirstSearch())
-# print(bst.breadthFirstSearchRecursive([bst.root],[]))
-print(bst.depthFirstSearchInOrder())
-# print(bst.depthFirstSearchPreOrder())
-# print(bst.depthFirstSearchPostOrder())
-
-
+unsorted_arr = [7,2,1,8,6,3,5,4]
+unsorted_arr1 = [22,11,88,66,55,77,33,44]
+unsorted_arr2 = [7,6,10,5,9,2,1,15,7]
+print(quickSort(unsorted_arr,0,len(unsorted_arr)-1))
