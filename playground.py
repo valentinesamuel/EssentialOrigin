78cd6991478f35fe5fe288e6f2487fa9b19622a2
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-05 10:38:22 +0100

    FEAT: Practiced mergesort

--- playground.py
+++ playground.py
@@ -21,7 +21,37 @@ def partition(arr, left, right):
     return part_at
 
 
-unsorted_arr = [7,2,1,8,6,3,5,4]
-unsorted_arr1 = [22,11,88,66,55,77,33,44]
-unsorted_arr2 = [7,6,10,5,9,2,1,15,7]
-print(quickSort(unsorted_arr,0,len(unsorted_arr)-1))
+# unsorted_arr = [7,2,1,8,6,3,5,4]
+# unsorted_arr1 = [22,11,88,66,55,77,33,44]
+# unsorted_arr2 = [7,6,10,5,9,2,1,15,7]
+# print(quickSort(unsorted_arr,0,len(unsorted_arr)-1))
+
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left = mergeSort(arr[:mid])
+    right = mergeSort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+
+    result += left[i:]
+    result += right[j:]
+
+    return result
+
+print(mergeSort([99,44,6,2,1,5,63,87,283,4,0]))
+print(mergeSort([13, 4, 9, 5, 3, 16, 12]))
