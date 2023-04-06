c8f181c710cf6408fb31876c6c73cc2f768cb26e
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-06 13:35:41 +0100

    added unmemoized fib

--- playground.py
+++ playground.py
@@ -11,6 +11,12 @@ def memoised_fib():
                 return cache[n]
     return fib
 
+def unmemoised_fib(n):
+    if n < 2:
+        return n
+    return unmemoised_fib(n-1) + unmemoised_fib(n-2)
+    
 
 fastFib = memoised_fib()
 print(fastFib(15))
+print(unmemoised_fib(15))
