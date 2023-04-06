853a8dda72120a8bf4ad89b22f16fb11d145658a
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-06 13:29:58 +0100

    FEAT; tested dp

--- playground.py
+++ playground.py
@@ -1,56 +1,16 @@
-def quickSort(arr, left, right):
-    if len(arr) <= 1:
-        return arr
-    
-    if left < right:
-        partition_idx = partition(arr, left, right)
-        left = quickSort(arr, left, partition_idx-1)
-        right = quickSort(arr, partition_idx+1, right)
-
-    return arr
-
-def partition(arr, left, right):
-    pivot = arr[right]
-    part_at = left
-
-    for j in range(left, right):
-        if arr[j] < pivot:
-            arr[part_at], arr[j] = arr[j], arr[part_at]
-            part_at += 1
-    arr[part_at], arr[right] = arr[right], arr[part_at]
-    return part_at
-
-
-# unsorted_arr = [7,2,1,8,6,3,5,4]
-# unsorted_arr1 = [22,11,88,66,55,77,33,44]
-# unsorted_arr2 = [7,6,10,5,9,2,1,15,7]
-# print(quickSort(unsorted_arr,0,len(unsorted_arr)-1))
-
-def mergeSort(arr):
-    if len(arr) <= 1:
-        return arr
-    
-    mid = len(arr) // 2
-    left = mergeSort(arr[:mid])
-    right = mergeSort(arr[mid:])
-
-    return merge(left, right)
-
-def merge(left, right):
-    result = []
-    i, j = 0, 0
-
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i+=1
+def memoised_fib():
+    cache = {}
+    def fib(n):
+        if n in cache:
+            return cache[n]
         else:
-            result.append(right[j])
-            j+=1
+            if n < 2:
+                return n
+            else:
+                cache[n] = fib(n-1) + fib(n-2)
+                return cache[n]
+    return fib
 
-    result += left[i:]
-    result += right[j:]
-    return result
 
-print(mergeSort([99,44,6,2,1,5,63,87,283,4,0]))
-print(mergeSort([13, 4, 9, 5, 3, 16, 12]))
+fastFib = memoised_fib()
+print(fastFib(15))
