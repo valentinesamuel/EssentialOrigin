5d193f208d592598663dff7a91cab910753bbb48
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-06 08:42:48 +0100

    cleared file

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-def selectionSort(arr):
-	for i in range(len(arr)):
-		min = i
-		for j in range(i + 1, len(arr)):
-			if arr[min] > arr[j]:
-				min = j
-		arr[min], arr[i] = arr[i], arr[min]
-	return arr
-
-
-def insertionSort(arr):
-	for i in range(1, len(arr)):
-		min = i
-		while min > 0 and arr[min - 1] > arr[min]:
-			arr[min], arr[min - 1] = arr[min - 1], arr[min]
-			min -= 1
-	return arr
-
-
-def bubbleSort(arr):
-	for i in range(len(arr)):
-		for j in range(len(arr) - i - 1):
-			if arr[j] > arr[j + 1]:
-				arr[j], arr[j + 1] = arr[j + 1], arr[j]
-	return arr
 
 
 lst = [2, 4, 7, 2, 6, 8, 12, 1, 0]
