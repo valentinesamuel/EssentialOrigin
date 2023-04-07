93f7681b8e42f090112551f486be644509cb4ab4
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-07 14:04:16 +0100

    FEAT: Aded DP file

---
+++ test_algo/dynamic_programming.py
@@ -0,0 +1,12 @@
+def memoized_fib():
+    cache = {}
+    def fib(n):
+        if n in cache:
+            return cache[n]
+        else:
+            if n < 2:
+                return n
+            else:
+                cache[n] = fib(n-1) + fib(n-2)
+                return cache[n]
+    return fib
